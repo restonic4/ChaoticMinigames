72515a4687f2436f8f7ccadddb85fd38f0964304
restonic4 <marcosruizlerma@gmail.com>
2025-01-07 23:50:32 +0100

    idk

--- src/main/java/com/chaotic_loom/chaotic_minigames/mixin/client/OptionsMixins.java
+++ src/main/java/com/chaotic_loom/chaotic_minigames/mixin/client/OptionsMixins.java
@@ -9,8 +9,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
